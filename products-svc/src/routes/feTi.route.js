f53f16eb5adc1324860b54559be8a241c3670751
AfrinAyesha <43309312+AfrinAyesha@users.noreply.github.com>
2024-03-29 17:06:17 +0400

    fix2

--- products-svc/src/routes/feTi.route.js
+++ products-svc/src/routes/feTi.route.js
@@ -2,11 +2,10 @@
 const express = require("express");
 const FeTiController = require("../controllers/feTi.controller");
 const auth = require("../shared/middlewares/auth.middleware");
-const route = require('../shared/middlewares/route.middleware')
 const router = express.Router();
 
 
-router.get("/health", route({ routeName: 'products:health' }), FeTiController.health);
+router.get("/health", FeTiController.health);
 router.post("/", auth({ routeName: 'products:addFeTiGrade' }), FeTiController.addFeTiGrade);
 router.get("/", auth({ routeName: 'products:getFeTiProducts' }), FeTiController.getFeTiProducts);
 // router.post("/:addressId",  AddressController.updateUserAddress);
